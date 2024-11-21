9919ae5d7fd87b08aa132c94404dbb8ae5d11405
NCuongLinh <143623013+NCuongLinh@users.noreply.github.com>
2024-11-22 06:26:35 +0700

    Chỉnh sửa nội dung

--- src/pages/Contact/FAQ.js
+++ src/pages/Contact/FAQ.js
@@ -53,7 +53,7 @@ function FAQ() {
             </AccordionSummary>
             <AccordionDetails>
               <Typography>
-                Trên GiveNow, bạn có thể lựa chọn các Dự án cộng đồng phù hợp với mong muốn của mình để ủng hộ. Các dự án trên GiveNow được thực hiện bởi các Tổ chức từ thiện, xã hội và môi trường có chuyên môn, uy tín và có chức năng tiếp nhận tài trợ theo quy định của pháp luật. Số tiền ủng hộ của bạn được chuyển thẳng tới các Tổ chức mà không thông qua bên thứ ba nào để đảm bảo sự minh bạch và hiệu quả.
+                Trên GiveHub, bạn có thể lựa chọn các Dự án cộng đồng phù hợp với mong muốn của mình để ủng hộ. Các dự án trên GiveHub được thực hiện bởi các Tổ chức từ thiện, xã hội và môi trường có chuyên môn, uy tín và có chức năng tiếp nhận tài trợ theo quy định của pháp luật. Số tiền ủng hộ của bạn được chuyển thẳng tới các Tổ chức mà không thông qua bên thứ ba nào để đảm bảo sự minh bạch và hiệu quả.
               </Typography>
             </AccordionDetails>
           </Accordion>
@@ -68,7 +68,7 @@ function FAQ() {
             </AccordionSummary>
             <AccordionDetails>
               <Typography>
-                Việc ủng hộ cho các dự án thiện nguyện trên GiveNow.vn tuyệt đối an toàn, tiền ủng hộ được thanh toán qua các cổng thanh toán uy tín như Viettel Pay, VnPay,..và tới trực tiếp tài khoản của các Tổ chức gây quỹ có tư cách pháp nhân, GiveNow chỉ ghi nhận và minh bạch hóa các thông tin.
+                Việc ủng hộ cho các dự án thiện nguyện trên GiveHub.vn tuyệt đối an toàn, tiền ủng hộ được thanh toán qua các cổng thanh toán uy tín như Viettel Pay, VnPay,..và tới trực tiếp tài khoản của các Tổ chức gây quỹ có tư cách pháp nhân, GiveHub chỉ ghi nhận và minh bạch hóa các thông tin.
               </Typography>
             </AccordionDetails>
           </Accordion>
@@ -83,7 +83,7 @@ function FAQ() {
             </AccordionSummary>
             <AccordionDetails>
               <Typography>
-                GiveNow hợp tác với các đối tác trung gian thanh toán và tích hợp đa dạng các phương thức thanh toán khác nhau để thuận tiện nhất cho người ủng hộ, bạn có thể lựa chọn bất cứ phương thức thanh toán nào phù hợp nhất với mình. Các phương thức thanh toán bao gồm:<br /><br />
+                GiveHub hợp tác với các đối tác trung gian thanh toán và tích hợp đa dạng các phương thức thanh toán khác nhau để thuận tiện nhất cho người ủng hộ, bạn có thể lựa chọn bất cứ phương thức thanh toán nào phù hợp nhất với mình. Các phương thức thanh toán bao gồm:<br /><br />
 
                 1. Thanh toán qua ứng dụng: Viettel Money, VnPay<br />
                 2. Thanh toán qua quét mã QR ViettelPay, VnPay<br />
@@ -102,7 +102,7 @@ function FAQ() {
             </AccordionSummary>
             <AccordionDetails>
               <Typography>
-                GiveNow không thu bất cứ khoản phí nào từ người ủng hộ. Người ủng hộ chỉ mất phí tiện ích/ phí dịch vụ (phí chuyển tiền thông thường) cho các nền tảng trung gian thanh toán được quy định bởi các bên:<br /><br />
+                GiveHub không thu bất cứ khoản phí nào từ người ủng hộ. Người ủng hộ chỉ mất phí tiện ích/ phí dịch vụ (phí chuyển tiền thông thường) cho các nền tảng trung gian thanh toán được quy định bởi các bên:<br /><br />
 
                 <b>ViettelPay:</b><br /><br />
 
@@ -111,10 +111,10 @@ function FAQ() {
 
                 <b>VnPay:</b><br /><br />
 
-                <li className='ml-3'>Thẻ nội địa/tài khoản ngân hàng/ví điện tử: Miễn phí dịch vụ 01 (một) năm kể từ 17/1/2022 (Kết thúc thời gian trên, GiveNow sẽ cập nhật mức phí do VnPay áp dụng);</li><br />
+                <li className='ml-3'>Thẻ nội địa/tài khoản ngân hàng/ví điện tử: Miễn phí dịch vụ 01 (một) năm kể từ 17/1/2022 (Kết thúc thời gian trên, GiveHub sẽ cập nhật mức phí do VnPay áp dụng);</li><br />
                 <li className='ml-3'>Thẻ quốc tế: 1.7% giá trị Giao dịch CTT + 2.200VND/Giao dịch CTT.</li><br /><br />
 
-                GiveNow nỗ lực hợp tác để mức phí này là nhỏ nhất so với mức phí chuyển tiền thông thường. Trong một số giai đoạn, phí chuyển tiền được các đối tác cổng thanh toán tài trợ miễn phí.<br />
+                GiveHub nỗ lực hợp tác để mức phí này là nhỏ nhất so với mức phí chuyển tiền thông thường. Trong một số giai đoạn, phí chuyển tiền được các đối tác cổng thanh toán tài trợ miễn phí.<br />
               </Typography>
             </AccordionDetails>
           </Accordion>
@@ -142,7 +142,7 @@ function FAQ() {
             </AccordionSummary>
             <AccordionDetails>
               <Typography>
-                Ngay sau khi chuyển tiền ủng hộ thành công cho một dự án thông qua các nền tảng thanh toán tích hợp trên GiveNow.vn, bạn sẽ thấy khoản ủng hộ của mình trong mục Danh sách ủng hộ của Dự án mà bạn chọn, bao gồm: Tên người ủng hộ (do bạn nhập thông tin) + Số tiền ủng hộ + Thời gian ủng hộ. Toàn bộ số tiền ủng hộ của bạn được chuyển thẳng tới tài khoản của Tổ chức gây quỹ mà không qua bên thứ ba nào khác.
+                Ngay sau khi chuyển tiền ủng hộ thành công cho một dự án thông qua các nền tảng thanh toán tích hợp trên GiveHub.vn, bạn sẽ thấy khoản ủng hộ của mình trong mục Danh sách ủng hộ của Dự án mà bạn chọn, bao gồm: Tên người ủng hộ (do bạn nhập thông tin) + Số tiền ủng hộ + Thời gian ủng hộ. Toàn bộ số tiền ủng hộ của bạn được chuyển thẳng tới tài khoản của Tổ chức gây quỹ mà không qua bên thứ ba nào khác.
               </Typography>
             </AccordionDetails>
           </Accordion>
@@ -156,7 +156,7 @@ function FAQ() {
             </AccordionSummary>
             <AccordionDetails>
               <Typography>
-                Ngoài ghi nhận đóng góp bằng tiền, GiveNow cho phép người dùng đóng góp bằng công sức thông qua chức năng Sứ giả truyền thông gây quỹ và ghi nhận các thông tin cần thiết một cách minh bạch. Hoặc bạn cũng có thể sử dụng nút chia sẻ để lan tỏa dự án thiện nguyện tới bạn bè của mình trên các mạng xã hội.
+                Ngoài ghi nhận đóng góp bằng tiền, GiveHub cho phép người dùng đóng góp bằng công sức thông qua chức năng Sứ giả truyền thông gây quỹ và ghi nhận các thông tin cần thiết một cách minh bạch. Hoặc bạn cũng có thể sử dụng nút chia sẻ để lan tỏa dự án thiện nguyện tới bạn bè của mình trên các mạng xã hội.
               </Typography>
             </AccordionDetails>
           </Accordion>
@@ -171,7 +171,7 @@ function FAQ() {
             </AccordionSummary>
             <AccordionDetails>
               <Typography>
-                Bạn có thể ủng hộ cho các Dự án gây quỹ trên GiveNow.vn mà không cần phải đăng ký làm thành viên của GiveNow. Tuy nhiên việc đăng ký sẽ giúp bạn tương tác và cập nhật tốt hơn các thông tin về dự án và tổ chức mà bạn quan tâm, ủng hộ.
+                Bạn có thể ủng hộ cho các Dự án gây quỹ trên GiveHub.vn mà không cần phải đăng ký làm thành viên của GiveHub. Tuy nhiên việc đăng ký sẽ giúp bạn tương tác và cập nhật tốt hơn các thông tin về dự án và tổ chức mà bạn quan tâm, ủng hộ.
               </Typography>
             </AccordionDetails>
           </Accordion>
@@ -192,9 +192,9 @@ function FAQ() {
             </AccordionSummary>
             <AccordionDetails>
               <Typography>
-                GiveNow là một nền tảng hỗ trợ các hoạt động mang lại lợi ích cho cộng đồng và xã hội, các dự án khởi chạy trên GiveNow phải được thực hiện bởi các tổ chức có chức năng gây quỹ và tiếp nhận tài trợ nhằm mục tiêu phục vụ cho các hoạt động từ thiện, xã hội và môi trường, theo tôn chỉ, mục đích của tổ chức mình và theo quy định của pháp luật.<br /><br />
+                GiveHub là một nền tảng hỗ trợ các hoạt động mang lại lợi ích cho cộng đồng và xã hội, các dự án khởi chạy trên GiveHub phải được thực hiện bởi các tổ chức có chức năng gây quỹ và tiếp nhận tài trợ nhằm mục tiêu phục vụ cho các hoạt động từ thiện, xã hội và môi trường, theo tôn chỉ, mục đích của tổ chức mình và theo quy định của pháp luật.<br /><br />
 
-                Để được khởi tạo dự án gây quỹ trên GiveNow.vn, tổ chức cần ký hợp đồng sử dụng nền tảng công nghệ thông tin với GiveNow và hợp đồng sử dụng cổng thanh toán với các đối tác trung gian thanh toán của GiveNow
+                Để được khởi tạo dự án gây quỹ trên GiveHub.vn, tổ chức cần ký hợp đồng sử dụng nền tảng công nghệ thông tin với GiveHub và hợp đồng sử dụng cổng thanh toán với các đối tác trung gian thanh toán của GiveHub
               </Typography>
             </AccordionDetails>
           </Accordion>
@@ -208,7 +208,7 @@ function FAQ() {
             </AccordionSummary>
             <AccordionDetails>
               <Typography>
-                GiveNow là nền tảng cho phép các Tổ chức gây quỹ chủ động khởi tạo các dự án trên GiveNow.vn, các tổ chức có thể tự tạo dự án với các mục tiêu về thời gian gây quỹ, số tiền gây quỹ, nội dung và các thông tin liên quan.<br /><br />
+                GiveHub là nền tảng cho phép các Tổ chức gây quỹ chủ động khởi tạo các dự án trên GiveHub.vn, các tổ chức có thể tự tạo dự án với các mục tiêu về thời gian gây quỹ, số tiền gây quỹ, nội dung và các thông tin liên quan.<br /><br />
 
                 Tuy nhiên, để việc gây quỹ hiệu quả, các tổ chức không nên tạo quá 5 dự án gây quỹ trong cùng 1 thời điểm.
               </Typography>
@@ -224,7 +224,7 @@ function FAQ() {
             </AccordionSummary>
             <AccordionDetails>
               <Typography>
-                Các dự án gây quỹ trên Givenow.vn được đề nghị gây quỹ không quá 60 ngày/dự án, để đảm bảo việc tập trung và truyền thông cho dự án. Tùy từng trường hợp, thời gian gây quỹ có thể được kéo dài.
+                Các dự án gây quỹ trên GiveHub.vn được đề nghị gây quỹ không quá 60 ngày/dự án, để đảm bảo việc tập trung và truyền thông cho dự án. Tùy từng trường hợp, thời gian gây quỹ có thể được kéo dài.
               </Typography>
             </AccordionDetails>
           </Accordion>
@@ -238,9 +238,9 @@ function FAQ() {
             </AccordionSummary>
             <AccordionDetails>
               <Typography>
-                Nền tảng GiveNow tự động cập nhật các tình trạng cho nhà tài trợ và các sứ giả gây quỹ hay những người quan tâm tới dự án của tổ chức, khi dự án hoàn thành gây quỹ, kết thúc thời gian gây quỹ, nhập các báo cáo triển khai, giai ngân.<br /><br />
+                Nền tảng GiveHub tự động cập nhật các tình trạng cho nhà tài trợ và các sứ giả gây quỹ hay những người quan tâm tới dự án của tổ chức, khi dự án hoàn thành gây quỹ, kết thúc thời gian gây quỹ, nhập các báo cáo triển khai, giai ngân.<br /><br />
 
-                GiveNow hướng tới phát triển thêm các tính năng như xuất hóa đơn/phiếu thu điện tử cho Người ủng hộ, Gửi Cerificate cho người ủng hộ,…
+                GiveHub hướng tới phát triển thêm các tính năng như xuất hóa đơn/phiếu thu điện tử cho Người ủng hộ, Gửi Cerificate cho người ủng hộ,…
               </Typography>
             </AccordionDetails>
           </Accordion>
@@ -254,7 +254,7 @@ function FAQ() {
             </AccordionSummary>
             <AccordionDetails>
               <Typography>
-                Các khoản ủng hộ thông qua GiveNow sẽ được chuyển thẳng tới Tài khoản ngân hàng của các Tổ chức gây quỹ trong thời gian T+1 kể từ thời điểm người ủng hộ chuyển tiền.
+                Các khoản ủng hộ thông qua GiveHub sẽ được chuyển thẳng tới Tài khoản ngân hàng của các Tổ chức gây quỹ trong thời gian T+1 kể từ thời điểm người ủng hộ chuyển tiền.
               </Typography>
             </AccordionDetails>
           </Accordion>
@@ -268,7 +268,7 @@ function FAQ() {
             </AccordionSummary>
             <AccordionDetails>
               <Typography>
-                GiveNow cung cấp tài khoản quản trị cho các Tổ chức gây quỹ có thể quản lý:<br /><br />
+                GiveHub cung cấp tài khoản quản trị cho các Tổ chức gây quỹ có thể quản lý:<br /><br />
 
                 1. Dự án gây quỹ: Tạo dự án, sửa dự án<br />
                 2. Sứ giả gây quỹ: Phê duyệt, từ chối sứ giả<br />
@@ -289,7 +289,7 @@ function FAQ() {
             </AccordionSummary>
             <AccordionDetails>
               <Typography>
-                Bạn có thể ghi lại thông tin người ủng hộ và thay mặt họ thực hiện việc ủng hộ thông qua GiveNow.vn để cập nhật danh sách người ủng hộ lên trang dự án.
+                Bạn có thể ghi lại thông tin người ủng hộ và thay mặt họ thực hiện việc ủng hộ thông qua GiveHub.vn để cập nhật danh sách người ủng hộ lên trang dự án.
               </Typography>
             </AccordionDetails>
           </Accordion>
@@ -317,7 +317,7 @@ function FAQ() {
             </AccordionSummary>
             <AccordionDetails>
               <Typography>
-                Dự án sẽ tự động kết thúc khi hết thời gian gây quỹ hoặc số tiền gây quỹ đạt được mục tiêu. Trong trường hợp khác, bạn vui lòng liên hệ GiveNow để được hỗ trợ.
+                Dự án sẽ tự động kết thúc khi hết thời gian gây quỹ hoặc số tiền gây quỹ đạt được mục tiêu. Trong trường hợp khác, bạn vui lòng liên hệ GiveHub để được hỗ trợ.
               </Typography>
             </AccordionDetails>
           </Accordion>
@@ -345,7 +345,7 @@ function FAQ() {
             </AccordionSummary>
             <AccordionDetails>
               <Typography>
-                Để tuân thủ chính sách riêng tư, bảo mật, các tổ chức gây quỹ không thể tiếp cận thông tin nhà tài trợ. GiveNow chỉ cung cấp danh sách được nhà tài trợ đồng ý bao gồm: Tên nhà tài trợ, số tiền tài trợ, thời gian tài trợ. Đồng thời, các tổ chức gây quỹ có thể tiếp cận các phân tích về nhà tài trợ để phục vụ cho mục đích phi lợi nhuận của mình, bao gồm: Giới tính, độ tuổi, địa lý.
+                Để tuân thủ chính sách riêng tư, bảo mật, các tổ chức gây quỹ không thể tiếp cận thông tin nhà tài trợ. GiveHub chỉ cung cấp danh sách được nhà tài trợ đồng ý bao gồm: Tên nhà tài trợ, số tiền tài trợ, thời gian tài trợ. Đồng thời, các tổ chức gây quỹ có thể tiếp cận các phân tích về nhà tài trợ để phục vụ cho mục đích phi lợi nhuận của mình, bao gồm: Giới tính, độ tuổi, địa lý.
               </Typography>
             </AccordionDetails>
           </Accordion>
@@ -360,7 +360,7 @@ function FAQ() {
             </AccordionSummary>
             <AccordionDetails>
               <Typography>
-                Bạn hoàn toàn có thể và GiveNow khuyến khích bạn chia sẻ và lan tỏa Dự án của mình tới bạn bè thông qua Facebook để đạt hiệu quả gây quỹ cao hơn. Biểu tượng chia sẻ được gắn ở mỗi dự án cụ thể.
+                Bạn hoàn toàn có thể và GiveHub khuyến khích bạn chia sẻ và lan tỏa Dự án của mình tới bạn bè thông qua Facebook để đạt hiệu quả gây quỹ cao hơn. Biểu tượng chia sẻ được gắn ở mỗi dự án cụ thể.
               </Typography>
             </AccordionDetails>
           </Accordion>
